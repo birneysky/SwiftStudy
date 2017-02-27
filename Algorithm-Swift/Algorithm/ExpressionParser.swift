14e43aa1883c482986756cfb94c6d8a0d89847a2
yingwei0831 <birneysky@163.com>
2017-02-27 22:34:49 +0800

    modiyf

--- Algorithm-Swift/Algorithm/ExpressionParser.swift
+++ Algorithm-Swift/Algorithm/ExpressionParser.swift
@@ -11,17 +11,27 @@ import Foundation
 
 extension Character{
     var isDigit: Bool{
-        if self > "0"  {
-            <#code#>
+        if self > "0" && self < "9"  {
+            return true
         }
-        return true;
+        return false
     }
+    
+    
 }
 
 class ExpressionParser{
     private var expression: String
     private var  numStack: Stack<String>
     private var  operatorStack: Stack<Character>
+    
+    indirect enum ArithmeticExpression{
+        case Number(Int)
+        case Addition(ArithmeticExpression,ArithmeticExpression)
+        case Multiplication(ArithmeticExpression,ArithmeticExpression)
+        case Subtraction(ArithmeticExpression,ArithmeticExpression)
+    }
+    
     
     init(expression: String){
         self.expression = expression
@@ -31,9 +41,23 @@ class ExpressionParser{
     
     
     func parse(){
+        var num: String = String();
+        var mathSymbol: String = String();
         for character in self.expression.characters{
-            print(character);
+        
+            if character.isDigit {
+                num.append(character)
+                //print(character)
+            }
+            else{
+                self.numStack.push(element: num);
+                num.removeAll();
+                mathSymbol.append(character)
+                self.operatorStack.push(element: character)
+            }
         }
+    
+        
     }
     
 }
